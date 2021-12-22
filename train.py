24c0286f5146b5c0da6456f6c84f72f922a329e4
Zhenyu Yang <jolynefr@O5-9-MBP.local>
2021-12-22 20:46:22 +0800

    correct validation_data

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 import random, os, sys
-from tqdm.keras import TqdmCallback
 from keras.datasets import cifar10
 from keras.utils import np_utils
 from keras.optimizer_v2.adam import Adam
@@ -14,16 +13,32 @@ from tensorflow.keras.utils import plot_model
 
 from ToyNet.model import ToyNet20
 
-batch_size = 32
 epochs = 100
 
 # enlarge dataset
 datagen = ImageDataGenerator(
-    rotation_range=0,
+    rotation_range=15,
     width_shift_range=0.1,
     height_shift_range=0.1,
     horizontal_flip=True,
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    validation_split=0.2
 )
+
+def lr_schedule(epoch):
+    lrate = 5e-4
+    if epoch > 90:
+        lrate *= 2e-4
+    elif epoch > 80:
+        lrate *= 5e-4
+    elif epoch > 65:
+        lrate *= 1e-3
+    elif epoch > 50:
+        lrate *= 1e-2
+    elif epoch > 30:
+        lrate *= 1e-1
+    return lrate
 
 def set_seed(seed=42):
     random.seed(seed)
@@ -34,13 +49,6 @@ def set_seed(seed=42):
 
 def load_processed_cifar10():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
-
-    # z-score
-    mean = np.mean(x_train, axis=0)
-    x_train = (x_train - mean)
-    x_test = (x_test - mean)
 
     # categorical
     y_train = np_utils.to_categorical(y_train, 10)
@@ -52,38 +60,17 @@ def training_callbacks(case_name):
     ckpt_path = f'training/toynet20/{case_name}/ckpt'
     log_path = f'training/toynet20/{case_name}/log'
 
-    def lr_schedule(epoch):
-        lrate = 1e-3
-        if epoch > 90:
-            lrate *= 2e-4
-        elif epoch > 80:
-            lrate *= 5e-4
-        elif epoch > 70:
-            lrate *= 1e-3
-        elif epoch > 55:
-            lrate *= 1e-2
-        elif epoch > 40:
-            lrate *= 1e-1
-        return lrate
-
     # checkpoint = ModelCheckpoint(filepath=ckpt_path,
     #                          monitor='accuracy',
     #                          verbose=1,
     #                          save_freq=10,
     #                          save_best_only=True)
 
-    lr_scheduler = LearningRateScheduler(lr_schedule)
-
-    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
-                               cooldown=0,
-                               patience=5,
-                               min_lr=0.5e-6)
-
-    tensorboard = TensorBoard(log_dir=log_path, histogram_freq=1)
-
-    tqdm_process = TqdmCallback(verbose=2)
-
-    return [lr_reducer, lr_scheduler, tensorboard, tqdm_process]
+    return [
+        ReduceLROnPlateau(patience=5, min_lr=0.5e-6), 
+        LearningRateScheduler(lr_schedule), 
+        TensorBoard(log_dir=log_path, histogram_freq=1),
+    ]
 
 if __name__ == '__main__':
 
@@ -102,15 +89,15 @@ if __name__ == '__main__':
     model.compile(loss='categorical_crossentropy',
                     optimizer=Adam(), metrics=['accuracy'])
     model.summary()
-    model.save_weights('model.h5') 
+    model.save_weights(f'training/toynet20/{case_name}/model.h5') 
     plot_model(model)
 
     callbacks=training_callbacks(case_name)
     datagen.fit(x_train)
     
-    model.fit(x=datagen.flow(x_train, y_train, batch_size=batch_size), 
-        epochs=epochs, validation_data=(x_test, y_test), 
-        verbose=0, callbacks=callbacks)
+    model.fit(x=datagen.flow(x_train, y_train, subset='training'), 
+        validation_data=datagen.flow(x_train, y_train, subset='validation'), 
+        verbose=1, callbacks=callbacks, epochs=epochs)
 
-    scores = model.evaluate(x_test, y_test, verbose=1)
-    print('\nTest result: %.3f loss: %.3f' % (scores[1]*100,scores[0]))
+    scores = model.evaluate(datagen.flow(x_test, y_test), verbose=1)
+    print('\nTest result: %.3f loss: %.3f' % (scores[1] * 100,scores[0]))
